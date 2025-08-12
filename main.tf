0cb20ecc4d3a9442d3c152822921483e48bc6c05
calebndavidson-del <calebndavidson@gmail.com>
2025-08-11 23:38:15 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,4 @@ provider "github" {
 
 resource "aws_s3_bucket" "lambda_deployment" {
   bucket = "cloud-trading-bot-lambda-deployment-m6x4p8e"
-  # Do NOT set force_destroy or try to set settings that would change the existing bucket unless you intend to
-  # Only specify arguments that match how the bucket is already configured, or leave blank/minimal
 }
