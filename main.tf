9a055f3d5a2d4cb105cac5834e6c816799bf0bc1
calebndavidson-del <calebndavidson@gmail.com>
2025-08-12 07:35:08 +0000

    commit

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "github" {
   owner = "calebndavidson-del"
 }
 
-resource "github_repository" "example" {
-  name        = "tf-example-repo"
-  description = "Created by Terraform!"
-  visibility  = "private"
+resource "aws_s3_bucket" "lambda_deployment" {
+  bucket = "cloud-trading-bot-lambda-deployment-m6x4p8e"
+  # Do NOT set force_destroy or try to set settings that would change the existing bucket unless you intend to
+  # Only specify arguments that match how the bucket is already configured, or leave blank/minimal
 }
